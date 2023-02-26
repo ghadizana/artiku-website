7412bbe8050001823cb8bf006df9d0eff277710f
ghadizana <ghadizanauraaliya@gmail.com>
2023-02-26 13:08:22 +0700

    "second commit"

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ArticleRequest;
+use App\Models\Articles;
+use Illuminate\Support\Facades\DB;
 
 class ArticleController extends Controller
 {
-    //
+    public function index (Request $request) 
+    {
+        if($request->search) {
+            $article = Articles::where('article', 'LIKE', "%$request->search%")->paginate(10);
+
+            return view('welcome', [
+                'data' => $article
+            ]);
+        };
+        
+        $article = Articles::paginate(10);
+        return view('welcome', [
+            'data' => $article
+        ]);
+    }
+
+    
 }
